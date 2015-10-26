a77e363994330d852a585a58f33342beff075932
toor <toor@cisco-1>
2015-10-26 14:28:41 +0100

    misc updates & mocks

---
+++ physical/src/main/java/vax/math/Plane3f.java
@@ -0,0 +1,14 @@
+package vax.math;
+
+import vax.math.Vector3f;
+
+/**
+
+ @author toor
+ */
+public class Plane3f {
+
+    public Plane3f ( Vector3f vector3f, float shiftX ) {
+    }
+
+}
---
+++ physical/src/main/java/vax/physical/body/SphereBody.java
@@ -0,0 +1,35 @@
+package vax.physical.body;
+
+import vax.math.Matrix4f;
+import vax.math.Vector3f;
+
+/**
+
+ @author toor
+ */
+public class SphereBody {
+
+    public SphereBody ( int i, float BALL_RADIUS ) {
+    }
+
+    public Matrix4f getTransform () {
+        throw new UnsupportedOperationException( "Not yet implemented." );
+    }
+
+    public void setRotationSpeed ( float f ) {
+        throw new UnsupportedOperationException( "Not yet implemented." );
+    }
+
+    public int getRestitution () {
+        throw new UnsupportedOperationException( "Not yet implemented." );
+    }
+
+    public void setRestitution ( int i ) {
+        throw new UnsupportedOperationException( "Not yet implemented." );
+    }
+
+    public Vector3f getVelocity () {
+        throw new UnsupportedOperationException( "Not yet implemented." );
+    }
+
+}
