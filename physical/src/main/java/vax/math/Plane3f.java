b4b9e8fafee0992b9643986ac6812a5cda8dbba7
mac <mac@MacBook-Air-Mac.local>
2015-10-27 18:37:53 +0100

    Updated Vectors and Plane in Math

--- physical/src/main/java/vax/math/Plane3f.java
+++ physical/src/main/java/vax/math/Plane3f.java
@@ -1,14 +1,41 @@
 package vax.math;
 
-import vax.math.Vector3f;
-
-/**
-
- @author toor
- */
 public class Plane3f {
-
-    public Plane3f ( Vector3f vector3f, float shiftX ) {
+    public Vector3f normal;
+    
+    public void setNormal( Vector3f vector ) {
+        this.normal = vector;
+    }
+    
+    public Vector3f getNormal() {
+        return this.normal;
     }
 
+    public float distanceToOrigin;
+    
+    public void setDistanceToOrigin( float dist ) {
+        this.distanceToOrigin = dist;
+    }
+    
+    public float getDistanceToOrigin() {
+        return this.distanceToOrigin;
+    }
+
+    public Plane3f() {
+        this(new Vector3f( 0, 1, 0 ), 0 );
+    }
+
+    public Plane3f ( Vector3f vector, float dist ) {
+        normal = vector;
+        distanceToOrigin = dist;
+    }
+
+    public float getDistance ( Vector3f point ) {
+        return normal.dot( point ) + distanceToOrigin;
+    }
+
+    public float getDistance ( Matrix4f transform ) {
+        return normal.dot( transform.TranslationX, transform.TranslationY, transform.TranslationZ ) + distanceToOrigin;
+    }
 }
+
--- physical/src/main/java/vax/math/Value1f.java
+++ physical/src/main/java/vax/math/Value1f.java
@@ -1,21 +1,22 @@
 package vax.math;
 
 public class Value1f {
-    private float value;
+    public float value;
+
+    public final void setValue(float val) {
+        this.value = val;
+    }
+
+    public float getValue() {
+        return this.value;
+    }
 
     public Value1f () {
     }
 
-    public Value1f ( float value ) {
-        this.value = value;
+    public Value1f ( float val ) {
+        setValue( val );
     }
-
-    public void setValue ( float value ) {
-        this.value = value;
-    }
-
-    public float getValue () {
-        return value;
-    }
-
 }
+
+
