ddc5c66aab47dd84721eba25ba25d130c8937c54
jakub_sengerski <jakub.sengerski@gmail.com>
2015-10-28 01:38:54 +0100

    initial physics commit

---
+++ physical/src/main/java/vax/physics/CenteredBody.java
@@ -0,0 +1,30 @@
+﻿package vax.physics;
+
+import vax.math.*;
+
+abstract public class CenteredBody extends Body {
+
+    public float radius;
+
+    public CenteredBody ( float mass, float radius ){
+        super(mass);
+        this.radius = radius;
+    }
+
+    public CenteredBody ( float mass, float radius, Matrix4f initialTransform ) {
+        super( mass, initialTransform );
+        this.radius = radius;
+    }
+
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+
+}
+
