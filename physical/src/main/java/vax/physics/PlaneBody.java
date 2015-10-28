aa477ccfbf8dc7fb303df1cfb78dfc42633e72c5
jakub_sengerski <jakub.sengerski@gmail.com>
2015-10-28 01:38:54 +0100

    initial physics commit

---
+++ physical/src/main/java/vax/physics/PlaneBody.java
@@ -0,0 +1,30 @@
+﻿package vax.physics;
+
+import vax.math.*;
+
+public class PlaneBody extends Body {
+
+    protected Plane3f plane3f;
+
+    public PlaneBody ( Plane3f plane3f ) {
+        this.plane3f = plane3f;
+    }
+
+    public PlaneBody () {
+        this(new Plane3f());
+    }
+
+    public PlaneBody ( Vector3f normal, float distanceToOrigin ) {
+        this( new Plane3f( normal, distanceToOrigin ) );
+    }
+
+    public Plane3f getPlane3f() {
+        return plane3f;
+    }
+
+    public void setPlane3f(Plane3f plane3f) {
+        this.plane3f = plane3f;
+    }
+}
+
+
---
+++ physical/src/main/java/vax/physics/SphereBody.java
@@ -0,0 +1,75 @@
+﻿package vax.physics;
+
+import vax.math.*;
+
+public class SphereBody extends CenteredBody {
+    protected float rotationAngle;
+    protected float rotationSpeed;
+    protected int rotationAxis;
+    protected float rotationVelocityThreshold;
+
+    public SphereBody(float mass, float radius) {
+        super(mass, radius);
+        restitution =  0.75f;
+        rotationVelocityThreshold = 0.15f;
+    }
+
+    public Matrix4f createRotationMatrix4() {
+        switch (rotationAxis) {
+            case Vector3f.OX:
+                return Matrix4f.createRotationX(rotationAngle);
+            case Vector3f.OY:
+                return Matrix4f.createRotationY(rotationAngle);
+            case Vector3f.OZ:
+                return Matrix4f.createRotationZ(rotationAngle);
+        }
+        throw new IllegalArgumentException();
+    }
+
+
+    @Override
+    public void timeStep(float deltaT) {
+        super.timeStep(deltaT);
+        float v = velocity.length();
+        if (v < rotationVelocityThreshold)
+            return;
+        //Console.WriteLine( Velocity.length() );
+            /*
+            rotationAngle += v * deltaT * rotationSpeed;
+            transform.setScaleAndRotation( createRotationMatrix4() );
+            */
+    }
+
+    public float getRotationAngle() {
+        return rotationAngle;
+    }
+
+    public void setRotationAngle(float rotationAngle) {
+        this.rotationAngle = rotationAngle;
+    }
+
+    public float getRotationSpeed() {
+        return rotationSpeed;
+    }
+
+    public void setRotationSpeed(float rotationSpeed) {
+        this.rotationSpeed = rotationSpeed;
+    }
+
+    public int getRotationAxis() {
+        return rotationAxis;
+    }
+
+    public void setRotationAxis(int rotationAxis) {
+        this.rotationAxis = rotationAxis;
+    }
+
+    public float getRotationVelocityThreshold() {
+        return rotationVelocityThreshold;
+    }
+
+    public void setRotationVelocityThreshold(float rotationVelocityThreshold) {
+        this.rotationVelocityThreshold = rotationVelocityThreshold;
+    }
+}
+
