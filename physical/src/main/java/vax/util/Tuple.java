500a667e39b3aef79243b6e8241f9d3c1ff1a111
jakub_sengerski <jakub.sengerski@gmail.com>
2015-10-30 00:49:22 +0100

    complete physics

---
+++ physical/src/main/java/vax/util/Tuple.java
@@ -0,0 +1,12 @@
+package vax.util;
+
+public class Tuple<T> {
+    public T left;
+    public T right;
+
+    public Tuple ( T t1, T t2){
+        this.left = t1;
+        this.right = t2;
+    }
+
+}
