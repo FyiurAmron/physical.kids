33a94def17ac03cd304f59096a916879a9e9dbc0
michal <michal@localhost>
2015-11-01 21:13:00 +0100

    Adding Math

---
+++ physical/src/main/java/vax/math/Matrix4.java
@@ -0,0 +1,29 @@
+package vax.math;
+
+/**
+ *
+ * @author mac
+ */
+class Matrix4 {
+    float M11;
+    float M12;
+    float M13;
+    float M14;
+    float M21;
+    float M22;
+    float M23;
+    float M24;
+    float M31;
+    float M32;
+    float M33;
+    float M34;
+    float M41;
+    float M42;
+    float M43;
+    float M44;
+
+    Matrix4( float data, float data0, float data1, float data2, float data3, float data4, float data5, float data6, float data7, float data8, float data9, float data10, float data11, float data12, float data13, float data14 ) {
+        throw new UnsupportedOperationException( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
--- physical/src/main/java/vax/math/Vector2f.java
+++ physical/src/main/java/vax/math/Vector2f.java
@@ -1,9 +1,48 @@
 package vax.math;
 
-/**
-
- @author toor
- */
 public class Vector2f extends VectorFloat {
 
+    public final static int SIZE = 2;
+
+    public float x;
+
+    public void setX( float val ) {
+        data[0] = val;
+    }
+
+    public float getX() {
+        return data[0];
+    }
+
+    public float y;
+
+    public void setY( float val ) {
+        data[1] = val;
+    }
+
+    public float getY() {
+        return data[1];
+    }
+
+    public Vector2f( Vector2f vector ) {
+        super( vector );
+    }
+
+    public Vector2f( float[] data ) {
+        super( data, SIZE );
+    }
+
+    public Vector2f() {
+        super( new float[SIZE] );
+    }
+
+    public Vector2f( float x, float y ) {
+        super( new float[] { x, y });
+    }
+
+    public Vector2f set( float x, float y ) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
 }
