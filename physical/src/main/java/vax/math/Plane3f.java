e9e1d57dddb958f0afbcf6b5b7d79ebe835a9953
masq3 <masq3@localhost>
2015-11-29 20:08:14 +0100

    Updated Plane with intersect

--- physical/src/main/java/vax/math/Plane3f.java
+++ physical/src/main/java/vax/math/Plane3f.java
@@ -49,10 +49,41 @@ public class Plane3f {
         distanceToOrigin = -point1.dot( normal );
     }
 
-//    public Line3f intersect ( Plane3f plane ) {
-//        
-//        
-//        return new Line3f();
-//    }
+    public Line3f intersect ( Plane3f plane ) {
+        Vector3f direction = normal.cross( plane.getNormal() );
+        Vector3f point = intersection( this, plane, new Plane3f( direction, 0 ) );
+        return new Line3f();
+    }
+
+    public Vector3f intersection ( Plane3f plane1, Plane3f plane2, Plane3f plane3 ) {
+        float a1 = plane1.getNormal().getX();
+        float b1 = plane1.getNormal().getY();
+        float c1 = plane1.getNormal().getZ();
+        float d1 = plane1.getDistanceToOrigin();
+
+        float a2 = plane2.getNormal().getX();
+        float b2 = plane2.getNormal().getY();
+        float c2 = plane2.getNormal().getZ();
+        float d2 = plane2.getDistanceToOrigin();
+
+        float a3 = plane3.getNormal().getX();
+        float b3 = plane3.getNormal().getY();
+        float c3 = plane3.getNormal().getZ();
+        float d3 = plane3.getDistanceToOrigin();
+
+        float a23 = b2 * c3 - b3 * c2;
+        float b23 = c2 * a3 - c3 * a2;
+        float c23 = a2 * b3 - a3 * b2;
+        float determinant = a1 * a23 + b1 * b23 + c1 * c23;
+        if ( Math.abs( determinant ) < 1.0e-6 ) {
+            return null;
+        }
+
+        float r = (float) (1.0 / determinant);
+        return new Vector3f(
+                (-a23 * d1 - (c1 * b3 - c3 * b1) * d2 - (c2 * b1 - c1 * b2) * d3) * r,
+                (-b23 * d1 - (c3 * a1 - c1 * a3) * d2 - (c1 * a2 - c2 * a1) * d3) * r,
+                (-c23 * d1 - (b1 * a3 - b3 * a1) * d2 - (b2 * a1 - b1 * a2) * d3) * r );
+    }
 
 }
