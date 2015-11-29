158053a885047197cf95097c416662fc29a8ebc2
vaxquis <vaxquis@localhost>
2015-11-29 01:39:31 +0100

    preliminary fixes for f4a8349

--- physical/src/test/java/vax/physics/SphereSphereColliderTest.java
+++ physical/src/test/java/vax/physics/SphereSphereColliderTest.java
@@ -1,37 +1,31 @@
 package vax.physics;
 
-import org.junit.Test;
-
 import java.util.*;
 
-import java.sql.JDBCType;
-
+import org.junit.Test;
 import static org.junit.Assert.*;
+import vax.math.Vector3f;
 
 /**
- * Created by Kuba on 2015-11-27.
+ Created by Kuba on 2015-11-27.
  */
 public class SphereSphereColliderTest {
+    private static final int COLLISION_ITERATIONS = 1000;
 
     @Test
     public void testCollide () throws Exception {
-
         /*
-         *  body setting:
-         *
-         *  b1      :   0,0         0,0         0,0
-         *  b2      :   0,0         0,0         0,0
-         *  bX      :   1,0         0,0         0,0
-         *  bY      :   0,0         1,0         0,0
-         *  bZ      :   0,0         0,0         1,0
-         *  bR      :   2*rand      2*rand      2*rand
-         *
-         *
+         body setting:
+
+         b1 : 0,0 0,0 0,0
+         b2 : 0,0 0,0 0,0
+         bX : 1,0 0,0 0,0
+         bY : 0,0 1,0 0,0
+         bZ : 0,0 0,0 1,0
+         bR : 2*rand 2*rand 2*rand
          */
-
-
         SphereSphereCollider collider = new SphereSphereCollider();
-        float radius = 1f;
+        //float radius = 1f;
 
         SphereBody b1 = new SphereBody( 1f, 1f );
         SphereBody b2 = new SphereBody( 1f, 1f );
@@ -41,49 +35,46 @@ public class SphereSphereColliderTest {
         SphereBody bZ = new SphereBody( 1f, 1f );
         bX.getTransform().addTranslation( 1f, 0f, 0f );
         bY.getTransform().addTranslation( 0f, 1f, 0f );
-        bY.getTransform().addTranslation( 0f, 1f, 0f );
+        bZ.getTransform().addTranslation( 0f, 0f, 1f );
 
-
-        assertEquals( true, collider.collide( b1, b2 ) );
-        assertEquals( true, collider.collide( b1, bX ) );
-        assertEquals( true, collider.collide( b1, bY ) );
-        assertEquals( true, collider.collide( b1, bZ ) );
+        assertTrue( collider.collide( b1, b2 ) );
+        assertTrue( collider.collide( b1, bX ) );
+        assertTrue( collider.collide( b1, bY ) );
+        assertTrue( collider.collide( b1, bZ ) );
 
         SphereBody bR;
         Random rand = new Random();
-        for( int i = 0; i <= 10000; i++ ) {
-            float f1 = 3 * rand.nextFloat();
-            float f2 = 3 * rand.nextFloat();
-            float f3 = 3 * rand.nextFloat();
+        for( int i = 0; i <= COLLISION_ITERATIONS; i++ ) {
+            float[] f = {
+                3 * rand.nextFloat(),
+                3 * rand.nextFloat(),
+                3 * rand.nextFloat() };
             b1 = new SphereBody( 1f, 1f );
             bR = new SphereBody( 1f, 1f );
-            bR.getTransform().addTranslation( f1, f2, f3 );
-            float distance = (float) Math.sqrt( f1 * f1 + f2 * f2 + f3 * f3 );
-            try {
-                assertEquals( distance <= 2 * 1f ? true : false, collider.collide( b1, bR ) );
+            bR.getTransform().addTranslation( f );
+            float distance = Vector3f.length( f );
+//            try {
+            assertEquals( distance <= 2 * 1f, collider.collide( b1, bR ) );
 //                System.out.println( "WIN\t dist: " + distance + " \t coll: " + collider.collide( b1, bR ) + "\t b1: " + b1.getTransform() + "\t bR: " + bR.getTransform());
-            } catch (AssertionError ae) {
+//            } catch (AssertionError ae) {
 //                System.out.println( "LOSE\t dist: " + distance + " \t coll: " + collider.collide( b1, bR ) + "\t b1: " + b1.getTransform() + "\t bR: " + bR.getTransform());
-            }
+//            }
         }
 
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test( expected = NullPointerException.class )
     public void testCollideNullValue1 () {
-        SphereSphereCollider ssc = new SphereSphereCollider();
-        ssc.collide( new SphereBody( 1f, 1f ), null );
+        new SphereSphereCollider().collide( new SphereBody( 1f, 1f ), null );
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test( expected = NullPointerException.class )
     public void testCollideNullValue2 () {
-        SphereSphereCollider ssc = new SphereSphereCollider();
-        ssc.collide( null, new SphereBody( 1f, 1f ) );
+        new SphereSphereCollider().collide( null, new SphereBody( 1f, 1f ) );
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test( expected = NullPointerException.class )
     public void testCollideNullValue3 () {
-        SphereSphereCollider ssc = new SphereSphereCollider();
-        ssc.collide( null, null );
+        new SphereSphereCollider().collide( null, null );
     }
 }
