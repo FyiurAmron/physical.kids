474d80ea15e000c236a2df6d95938c3cda23c323
vaxquis <vaxquis@localhost>
2015-12-10 00:32:41 +0100

    misc encapsulation fixes

--- physical/src/main/java/vax/physics/TriangleBody.java
+++ physical/src/main/java/vax/physics/TriangleBody.java
@@ -7,7 +7,10 @@ import vax.math.Vector3f;
  Created by Kuba on 2015-12-02.
  */
 public class TriangleBody extends PlaneBody {
-    public Vector3f[] points = new Vector3f[3];
+    protected final Vector3f //
+            point1 = new Vector3f(),
+            point2 = new Vector3f(),
+            point3 = new Vector3f();
 
     /**
      No-arg for serialization/subclassing.
@@ -15,33 +18,56 @@ public class TriangleBody extends PlaneBody {
     public TriangleBody () {
     }
 
+    /**
+     equivalent to <code>new TriangleBody().setPoints(v1,v2,v3)</code>
+
+     @param v1
+     @param v2
+     @param v3
+     */
     public TriangleBody ( Vector3f v1, Vector3f v2, Vector3f v3 ) {
         _setPoints( v1, v2, v3 );
     }
 
-    public boolean equals ( TriangleBody tb ) {
-        return points[0].equals( tb.points[0] )
-                && points[1].equals( tb.points[1] )
-                && points[2].equals( tb.points[2] );
+    /**
+     Sets the outputs to values contained in this TriangleBody's corner points.
+
+     @param output1
+     @param output2
+     @param output3
+     */
+    public void getPoints ( Vector3f output1, Vector3f output2, Vector3f output3 ) {
+        output1.set( point1 );
+        output2.set( point2 );
+        output3.set( point3 );
     }
 
-    @Override
-    public int hashCode () {
-        throw new UnsupportedOperationException(); // TODO [Jakub Sengerski] - always implement hashCode() when implementing equals()
-    }
+    /**
+     Copies the values from source vectors into local data.
 
-    public Vector3f[] getPoints () {
-        return points;
-    }
-
+     @param v1
+     @param v2
+     @param v3
+     */
     public void setPoints ( Vector3f v1, Vector3f v2, Vector3f v3 ) {
         _setPoints( v1, v2, v3 );
     }
 
     private void _setPoints ( Vector3f v1, Vector3f v2, Vector3f v3 ) {
         setPlane3f( new Plane3f( v1, v2, v3 ) );
-        this.points[0] = v1;
-        this.points[1] = v2;
-        this.points[2] = v3;
+        point1.set( v1 );
+        point2.set( v2 );
+        point3.set( v3 );
+    }
+
+    public boolean equals ( TriangleBody tb ) {
+        return point1.equals( tb.point1 )
+                && point2.equals( tb.point2 )
+                && point3.equals( tb.point3 );
+    }
+
+    @Override
+    public int hashCode () {
+        throw new UnsupportedOperationException(); // TODO [Jakub Sengerski] - always implement hashCode() when implementing equals()
     }
 }
