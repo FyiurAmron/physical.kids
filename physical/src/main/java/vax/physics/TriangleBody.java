01cb6b13cbc9f43653d87a43bd0413d399cfed57
jakub_sengerski <jakub.sengerski@gmail.com>
2016-01-18 02:41:58 +0100

    added PolygonBody, Trangle/Rectangle colliders replaced with general PolygonCollider

--- physical/src/main/java/vax/physics/TriangleBody.java
+++ physical/src/main/java/vax/physics/TriangleBody.java
@@ -1,106 +1,22 @@
 package vax.physics;
 
-import vax.math.Plane3f;
 import vax.math.Vector3f;
 
 /**
  Created by Kuba on 2015-12-02.
  */
-public class TriangleBody extends Body {
-    protected final Vector3f //
-            point1 = new Vector3f(),
-            point2 = new Vector3f(),
-            point3 = new Vector3f();
-    protected Plane3f plane = new Plane3f();
+public class TriangleBody extends PolygonBody {
 
-    /**
-     No-arg for serialization/subclassing.
-     */
+    static {
+        verticesCount = 3;
+    }
+
     public TriangleBody () {
+        super();
     }
 
-    /**
-     equivalent to <code>new TriangleBody().setPoints(v1,v2,v3)</code>
-
-     @param v1
-     @param v2
-     @param v3
-     */
-    public TriangleBody ( Vector3f v1, Vector3f v2, Vector3f v3 ) {
-        _setPoints( v1, v2, v3 );
+    public TriangleBody ( Vector3f[] vArray ) {
+        super( vArray );
     }
 
-    /**
-     Sets the outputs to values contained in this TriangleBody's corner points.
-
-     @param output1
-     @param output2
-     @param output3
-     */
-    public void getPoints ( Vector3f output1, Vector3f output2, Vector3f output3 ) {
-        output1.set( point1 );
-        output2.set( point2 );
-        output3.set( point3 );
-    }
-
-    /**
-     Copies the values from source vectors into local data.
-
-     @param v1
-     @param v2
-     @param v3
-     */
-    public void setPoints ( Vector3f v1, Vector3f v2, Vector3f v3 ) {
-        _setPoints( v1, v2, v3 );
-    }
-
-    private void _setPoints ( Vector3f v1, Vector3f v2, Vector3f v3 ) {
-        point1.set( v1 );
-        point2.set( v2 );
-        point3.set( v3 );
-        plane.set( point1, point2, point3 );
-    }
-
-    /**
-     Copies local plane values to output.
-
-     @param output
-     @return reference to output, for chaining
-     */
-    public Plane3f getPlane ( Plane3f output ) {
-        output.set( plane );
-        return output;
-    }
-
-    /*
-    public Vector3f[] getPointsArray () {
-        return new Vector3f[]{
-                new Vector3f( point1 ),
-                new Vector3f( point2 ),
-                new Vector3f( point3 )
-        };
-    }
-*/
-
-    @Override
-    public boolean equals ( Object o ) {
-        if ( o == null )
-            return false;
-        if ( o == this )
-            return true;
-        if ( !(o instanceof TriangleBody) )
-            return false;
-
-        TriangleBody tb = (TriangleBody) o;
-
-        return point1.equals( tb.point1 ) && point2.equals( tb.point2 ) && point3.equals( tb.point3 );
-    }
-
-    @Override
-    public int hashCode () {
-        int result = point1.hashCode();
-        result = 31 * result + point2.hashCode();
-        result = 31 * result + point3.hashCode();
-        return result;
-    }
 }
